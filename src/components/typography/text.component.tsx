80a9a315c099f6bc674cd69fee85db1fcd9c85be
mehmetasilkilic <mehmetasil96@gmail.com>
2022-11-24 23:23:52 +0300

    custom safeArea and text components added

---
+++ src/components/typography/text.component.tsx
@@ -0,0 +1,86 @@
+import styled from "styled-components/native";
+
+type VariantsProps = {
+  titleSmall: any;
+  titleLarge: any;
+  titleMedium: any;
+  hint: any;
+  success: any;
+  error: any;
+  caption: any;
+  label: any;
+};
+
+const defaultTextStyles = (theme: any): string => `
+    font-family: ${theme.fonts.heading};
+    font-weight: ${theme.fontWeights.regular};
+    color: ${theme.colors.text.primary};
+    flex-wrap: wrap;
+    margin-top: 0px;
+    margin-bottom: 0px;
+`;
+
+const titleLarge = (theme: any): string => `
+    font-size: ${theme.fontSizes.h4};
+    font-weight: ${theme.fontWeights.bold};
+    color: ${theme.colors.text.white};
+`;
+
+const titleMedium = (theme: any): string => `
+    font-size: ${theme.fontSizes.h5};
+    font-weight: ${theme.fontWeights.bold};
+    color: ${theme.colors.text.inverse};
+`;
+
+const titleSmall = (theme: any): string => `
+    font-size: ${theme.fontSizes.title};
+    color: ${theme.colors.text.inverse};
+`;
+
+const hint = (theme: any): string => `
+    font-size: ${theme.fontSizes.button};
+    color: ${theme.colors.text.disabled};
+`;
+
+const success = (theme: any): string => `
+    color: ${theme.colors.text.success};
+    font-size: ${theme.fontSizes.button};
+`;
+
+const error = (theme: any): string => `
+    color: ${theme.colors.text.error};
+    font-size: ${theme.fontSizes.button};
+`;
+
+const caption = (theme: any): string => `
+    color: ${theme.colors.text.disabled};
+    font-size: ${theme.fontSizes.caption};
+`;
+
+const label = (theme: any): string => `
+    font-size: ${theme.fontSizes.body};
+`;
+
+const variants: VariantsProps = {
+  titleSmall,
+  titleLarge,
+  titleMedium,
+  hint,
+  success,
+  error,
+  caption,
+  label,
+};
+
+type TextProps = {
+  variant: any;
+};
+
+export const Text = styled.Text<TextProps>`
+  ${({ theme }) => defaultTextStyles(theme)}
+  ${({ variant, theme }) => variants[variant as keyof VariantsProps](theme)}
+`;
+
+Text.defaultProps = {
+  variant: "label",
+};
---
+++ src/components/utils/safeArea.component.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+import { StatusBar, SafeAreaView } from "react-native";
+
+export const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  ${StatusBar.currentHeight && `padding-top: ${StatusBar.currentHeight}px`}
+`;
+
+export const SafeAreaBottom = styled(SafeAreaView)`
+  flex: 0;
+  background-color: ${(props) => props.theme.colors.bg.secondary};
+`;
